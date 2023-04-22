8748a4da385c657a5f3b4d1e8c703df4eaa335bc
Андрій Шевельов <ashevelevmti@gmail.com>
2023-04-22 22:24:19 +0300

    fix modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 /** @format */
 
 import Sidebar from "../modules/sidebar";
-
+import { useAppSelector } from "@/redux/hooks";
 import useSearchParams from "../hooks/useSearchParams";
 import ListProducts from "../modules/list";
-
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { useQuery } from "@apollo/client";
 import { schemasGql } from "../gql";
 
 export default function Main() {
   const { params: query } = useSearchParams();
+  const isOpen = useAppSelector((store) => store.button.menu);
+  const matches = useMediaQuery("(min-width:480px)");
+  const visibility = matches ? true : !isOpen;
 
   const { loading, data } = useQuery(schemasGql.GET_PRODUCTS, {
     variables: { query },
@@ -20,7 +23,9 @@ export default function Main() {
   return (
     <div className="flex w-full ">
       <Sidebar />
-      <ListProducts data={results} count={count} isLoading={loading} />
+      {visibility && (
+        <ListProducts data={results} count={count} isLoading={loading} />
+      )}
     </div>
   );
 }
