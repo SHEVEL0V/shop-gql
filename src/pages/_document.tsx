5ef7f42bff266f5a09b68382b70031e7652d85b4
Андрій Шевельов <ashevelevmti@gmail.com>
2023-09-24 17:40:15 +0300

    add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head></Head>
-      <body>
+      <body className="font-sans">
         <Main />
         <NextScript />
       </body>
